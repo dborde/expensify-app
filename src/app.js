d05ebae73491461db486747afdb3809d19f22d12
David Borde <dborde@gogoair.com>
2018-07-15 13:47:15 -0500

    comment out store.scubscribe and dependencies

--- src/app.js
+++ src/app.js
@@ -5,18 +5,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import getVisibleExpenses from './selectors/expenses';
-import { addExpense } from './actions/expenses';
-import { setTextFilter, setStartDate, setEndDate } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter, setStartDate, setEndDate } from './actions/filters';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
 
 const jsx = (
   <Provider store={store}>
